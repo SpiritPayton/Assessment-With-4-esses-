4814e210a6dd687196363affb9dafb872eec0c1b
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2023-05-10 10:30:33 +1200

    vexscream

--- Search Function v3.py
+++ Search Function v3.py
@@ -1,7 +1,5 @@
-"""This is the search function to search for existing monster cards on the catalogue
-"""
-
 import easygui
+
 
 monster_dict = {
     "Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
@@ -18,22 +16,39 @@ monster_dict = {
 
 monster_list = list(monster_dict.keys())
 
+
+class MonsterCard:
+    def __init__(self, name, stats):
+        self.name = name
+        self.stats = stats
+
+    def __str__(self):
+        return f"{self.name}: {self.stats}"
+
+    def search(self):
+        search_term = easygui.enterbox("What monster are you searching for?", "Search")
+        if search_term:
+            matches = [monster for monster in monster_list if search_term.lower() in monster.lower()]
+            if matches:
+                selected_monster = easygui.choicebox("Select a monster:", "Search Results", matches)
+                if selected_monster:
+                    monster_details = monster_dict[selected_monster]
+                    return selected_monster, monster_details
+                else:
+                    easygui.msgbox("No monster selected.")
+            else:
+                easygui.msgbox(f"No matches found for '{search_term}'.")
+
+
 while True:
     option = easygui.buttonbox("Grrrrr! How can I help you with your Monster Card collection?",
                                "Monster Card Menu", choices=["Search", "Add", "Combos", "Explode"])
     # if they wanna search
     if option == "Search":
-        # ask the user to select a monster to search for
-        monster_choice = easygui.choicebox("What monster are you searching for?",
-                                           "Monster Card Search", choices=monster_list)
-        # if the user selects a monster, show its details
-        if monster_choice:
-            monster_details = monster_dict[monster_choice]
-            details_str = "\n".join([f"{k}: {v}" for k, v in monster_details.items()])
-            easygui.msgbox(details_str)
-            # if a combo is found show details & price
-        if details:
-            monster_name = details[0]
-            monster_details = details[1]
+        # create a MonsterCard object and call its search method to find a monster
+        monster = MonsterCard("", {}).search()
+        # if a monster is found, show its details
+        if monster:
+            monster_name, monster_details = monster
             details_str = "\n".join([f"{k}: {v}" for k, v in monster_details.items()])
             easygui.msgbox(details_str, title=monster_name)
